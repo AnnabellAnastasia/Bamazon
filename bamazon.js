807b4c7f9b2899a4c77151fefbac3ea79f88de9c
AnnabellAnastasia <oav2008@gmail.com>
2019-05-16 23:23:55 -0400

    create question

--- bamazon.js
+++ bamazon.js
@@ -16,7 +16,25 @@ connection.connect(function(err){
 var makeTable = function(){
     connection.query("SELECT*FROM products", function(err,res){
         for(var i=0; i<res.length;i++){
-            console.log(res[i].item)
+            console.log(res[i].item+" || " +res[i].productname+" || "+ res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquantity+"\n");
+
+        }
+        promptCustomer(res);
+    })
+}
+var promptCustomer = function(res){
+    inquirer.prompt([{
+        type:'input',
+        name:'choise',
+        message:"What would you like to purchase? [Quit with Q}"
+    }]).then(function(answer){
+        var correct = false;
+        for(var i = 0;i,res.length;i++){
+            if(res[i].productname==answer.choice){
+                correct=true;
+                var productname=answer.choice;
+                var id=i;
+            }
         }
     })
 }
